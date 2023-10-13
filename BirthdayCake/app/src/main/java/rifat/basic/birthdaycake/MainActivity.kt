0e45f93d5bee26ca4105ecbf8dc9254327b4f6ba
Rifat Khadafy <rifat.khadafy@anteraja.id>
2023-10-13 21:16:04 +0700

    add birthday app

---
+++ BirthdayCake/app/src/main/java/rifat/basic/birthdaycake/MainActivity.kt
@@ -0,0 +1,11 @@
+package rifat.basic.birthdaycake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
